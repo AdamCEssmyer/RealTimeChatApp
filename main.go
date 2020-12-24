850115b717a151e62e4dbd0579f5d045219eff02
Adam Essmyer <acemyer@gmail.com>
2020-12-24 12:51:43 -0600

    created go file for server side

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var clients = make(map[*websocket.Conn]bool)
+var broadcast = make(chan Message)
+
+var upgrader = websocket.Upgrader{}
